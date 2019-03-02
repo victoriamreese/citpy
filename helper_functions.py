cb8f52aa07a3297dcaaff2cc5a17a325166026be
vicky <vickyreese@gmail.com>
2019-03-02 16:44:16 -0600

    create Leaf class

--- helper_functions.py
+++ helper_functions.py
@@ -1,22 +1,16 @@
 import math
 
-line1 = [3,2]
-line2 = [6,7]
-
-#calculate the length of a line segment
-
-    
-
-def return_major_axis(line1, line2):
-    return max(line1, line2)
-        
-
-
-
-
 
 class Leaf:
     def __init__(self, x1_a, y1_a, x2_a, y2_a, x1_b, y1_b, x2_b, y2_b):
+        self.x1_a = x1_a
+        self.y1_a = y1_a
+        self.x2_a = x2_a
+        self.y2_a = y2_a
+        self.x1_b = x1_b
+        self.y1_b = y1_b
+        self.x2_b = x2_b
+        self.y2_b = y2_b
         self.axis1 = [(x1_a, y1_a),(x2_a, y2_a)]
         self.axis2 = [(x1_b, y1_b), (x2_b, y2_b)]
         self.slope1 = abs(y1_a - y2_a)/abs(x1_a - x2_a) if abs(x1_a - x2_a) != 0 else 0.0
@@ -30,14 +24,26 @@ class Leaf:
         else:
             return ((math.atan(numerator/denominator)*180/math.pi))
     
-    def calc_lengths(self):
+    def calc_lengths_major_minor(self):
+        """
+        returns length of axes in order of size, 
+        [minor axis, major axis]
+        """
         import math
-        length = math.sqrt(ydif**2 + xdif**2)
-        return length_axis1, length_axis2
+        length_axis1 = math.sqrt(abs(self.y1_a - self.y2_a)**2 + abs(self.x1_a - self.x2_a)**2)
+        length_axis2 = math.sqrt(abs(self.y1_b - self.y2_b)**2 + abs(self.x1_b - self.x2_b)**2)
+        return sorted([length_axis1, length_axis2])
+    
+    def check_intersection(self):
+        #todo
+        pass
+        
+    
         
 
-leaf = Leaf(0,0,0,0,0,0,0,0)
-leaf.calc_angle_between_segments()
+leaf = Leaf(3,2,5,4,7,6,6,6)
+leaf.calc_lengths_major_minor()
+
         
 
 
