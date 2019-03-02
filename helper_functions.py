c40387256cdfc97a03f2b43db7207abbd71a81e9
William Booth <wbbooth@me.com>
2019-03-01 18:06:31 -0600

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 25 18:10:49 2019
-
-@author: vicky
-"""
-
-
 import math
-#calculate the length of a line segment
+
+
 def calc_length(x1, y1, x2, y2):
-    import math
+    """
+    calculate the length of a line segment
+    """
     ydif, xdif = abs(y2-y1), abs(x2-x1)
     slope = ydif/xdif
     length = math.sqrt(ydif**2 + xdif**2)
-    print(length)
+    return slope, length
     
 #compare length of two line segments and return larger one
 def which_bigger(line1, line2):
+    return max()
     if line1>line2:
         return line1
     else:
         return line2
+    
+    # other option for one-line
+    return line1 if line1>line2 else line2 
         
         
 #see if two line segments intersect
@@ -35,7 +33,7 @@ def if_intersect(line1, line2):
 #find angle between lines
 def angle_between(slope2, slope1):
     num = (slope2 - slope1)
-    denom = (1 + slope2*slope1)
+    denom = (1 + slope2 * slope1)
     if denom == 0:
         print(90.0)
     else:
